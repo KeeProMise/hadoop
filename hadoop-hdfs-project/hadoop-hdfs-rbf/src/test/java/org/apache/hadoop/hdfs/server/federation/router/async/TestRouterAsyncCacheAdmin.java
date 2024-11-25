6264c3e36bfc430177c2f70ed76620ede4c456a5
Jian Zhang <keepromise@apache.org>
2024-11-25 22:18:01 +0800

    use RouterAsyncProtocolTestBase

--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/test/java/org/apache/hadoop/hdfs/server/federation/router/async/TestRouterAsyncCacheAdmin.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/test/java/org/apache/hadoop/hdfs/server/federation/router/async/TestRouterAsyncCacheAdmin.java
@@ -18,130 +18,35 @@
 
 package org.apache.hadoop.hdfs.server.federation.router.async;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.CacheFlag;
 import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.protocol.CacheDirectiveEntry;
 import org.apache.hadoop.hdfs.protocol.CacheDirectiveInfo;
 import org.apache.hadoop.hdfs.protocol.CachePoolEntry;
 import org.apache.hadoop.hdfs.protocol.CachePoolInfo;
-import org.apache.hadoop.hdfs.server.federation.MiniRouterDFSCluster;
-import org.apache.hadoop.hdfs.server.federation.MockResolver;
-import org.apache.hadoop.hdfs.server.federation.RouterConfigBuilder;
 import org.apache.hadoop.fs.BatchedRemoteIterator.BatchedEntries;
-import org.apache.hadoop.hdfs.server.federation.router.RBFConfigKeys;
-import org.apache.hadoop.hdfs.server.federation.router.RouterRpcServer;
-import org.apache.hadoop.ipc.CallerContext;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.util.EnumSet;
-import java.util.concurrent.TimeUnit;
-
-import static org.apache.hadoop.hdfs.server.federation.FederationTestUtils.NAMENODES;
-import static org.apache.hadoop.hdfs.server.federation.MiniRouterDFSCluster.DEFAULT_HEARTBEAT_INTERVAL_MS;
-import static org.apache.hadoop.hdfs.server.federation.router.RBFConfigKeys.DFS_ROUTER_RPC_ASYNC_HANDLER_COUNT;
-import static org.apache.hadoop.hdfs.server.federation.router.RBFConfigKeys.DFS_ROUTER_RPC_ASYNC_RESPONDER_COUNT;
 import static org.apache.hadoop.hdfs.server.federation.router.async.utils.AsyncUtil.syncReturn;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-public class TestRouterAsyncCacheAdmin {
-  private static Configuration routerConf;
-  /** Federated HDFS cluster. */
-  private static MiniRouterDFSCluster cluster;
-  private static String ns0;
-
-  /** Random Router for this federated cluster. */
-  private MiniRouterDFSCluster.RouterContext router;
-  private FileSystem routerFs;
-  private RouterRpcServer routerRpcServer;
+/**
+ * Used to test the functionality of {@link RouterAsyncCacheAdmin}.
+ */
+public class TestRouterAsyncCacheAdmin extends RouterAsyncProtocolTestBase {
   private RouterAsyncCacheAdmin asyncCacheAdmin;
 
-  @BeforeClass
-  public static void setUpCluster() throws Exception {
-    cluster = new MiniRouterDFSCluster(true, 1, 2,
-        DEFAULT_HEARTBEAT_INTERVAL_MS, 1000);
-    cluster.setNumDatanodesPerNameservice(3);
-
-    cluster.startCluster();
-
-    // Making one Namenode active per nameservice
-    if (cluster.isHighAvailability()) {
-      for (String ns : cluster.getNameservices()) {
-        cluster.switchToActive(ns, NAMENODES[0]);
-        cluster.switchToStandby(ns, NAMENODES[1]);
-      }
-    }
-    // Start routers with only an RPC service
-    routerConf = new RouterConfigBuilder()
-        .rpc()
-        .build();
-
-    // Reduce the number of RPC clients threads to overload the Router easy
-    routerConf.setInt(RBFConfigKeys.DFS_ROUTER_CLIENT_THREADS_SIZE, 1);
-    routerConf.setInt(DFS_ROUTER_RPC_ASYNC_HANDLER_COUNT, 1);
-    routerConf.setInt(DFS_ROUTER_RPC_ASYNC_RESPONDER_COUNT, 1);
-    // We decrease the DN cache times to make the test faster
-    routerConf.setTimeDuration(
-        RBFConfigKeys.DN_REPORT_CACHE_EXPIRE, 1, TimeUnit.SECONDS);
-    cluster.addRouterOverrides(routerConf);
-    // Start routers with only an RPC service
-    cluster.startRouters();
-
-    // Register and verify all NNs with all routers
-    cluster.registerNamenodes();
-    cluster.waitNamenodeRegistration();
-    cluster.waitActiveNamespaces();
-    ns0 = cluster.getNameservices().get(0);
-  }
-
-  @AfterClass
-  public static void shutdownCluster() throws Exception {
-    if (cluster != null) {
-      cluster.shutdown();
-    }
-  }
-
   @Before
-  public void setUp() throws IOException {
-    router = cluster.getRandomRouter();
-    routerFs = router.getFileSystem();
-    routerRpcServer = router.getRouterRpcServer();
-    routerRpcServer.initAsyncThreadPool();
-    RouterAsyncRpcClient asyncRpcClient = new RouterAsyncRpcClient(
-        routerConf, router.getRouter(), routerRpcServer.getNamenodeResolver(),
-        routerRpcServer.getRPCMonitor(),
-        routerRpcServer.getRouterStateIdContext());
-    RouterRpcServer spy = Mockito.spy(routerRpcServer);
-    Mockito.when(spy.getRPCClient()).thenReturn(asyncRpcClient);
-    asyncCacheAdmin = new RouterAsyncCacheAdmin(spy);
-
-    // Create mock locations
-    MockResolver resolver = (MockResolver) router.getRouter().getSubclusterResolver();
-    resolver.addLocation("/", ns0, "/");
-    FSDataOutputStream fsDataOutputStream = routerFs.create(
+  public void setup() throws IOException {
+    asyncCacheAdmin = new RouterAsyncCacheAdmin(getRouterAsyncRpcServer());
+    FSDataOutputStream fsDataOutputStream = getRouterFs().create(
         new Path("/testCache.file"), true);
     fsDataOutputStream.write(new byte[1024]);
     fsDataOutputStream.close();
-  }
-
-  @After
-  public void tearDown() throws IOException {
-    // clear client context
-    CallerContext.setCurrent(null);
-    boolean delete = routerFs.delete(new Path("/testCache.file"));
-    assertTrue(delete);
-    if (routerFs != null) {
-      routerFs.close();
-    }
   }
 
   @Test
--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/test/java/org/apache/hadoop/hdfs/server/federation/router/async/TestRouterAsyncSnapshot.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/test/java/org/apache/hadoop/hdfs/server/federation/router/async/TestRouterAsyncSnapshot.java
@@ -17,133 +17,39 @@
  */
 package org.apache.hadoop.hdfs.server.federation.router.async;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.hdfs.protocol.SnapshotDiffReport;
 import org.apache.hadoop.hdfs.protocol.SnapshotDiffReportListing;
 import org.apache.hadoop.hdfs.protocol.SnapshotException;
 import org.apache.hadoop.hdfs.protocol.SnapshotStatus;
 import org.apache.hadoop.hdfs.protocol.SnapshottableDirectoryStatus;
-import org.apache.hadoop.hdfs.server.federation.MiniRouterDFSCluster;
-import org.apache.hadoop.hdfs.server.federation.MockResolver;
-import org.apache.hadoop.hdfs.server.federation.RouterConfigBuilder;
-import org.apache.hadoop.hdfs.server.federation.router.RBFConfigKeys;
-import org.apache.hadoop.hdfs.server.federation.router.RouterRpcServer;
-import org.apache.hadoop.ipc.CallerContext;
 import org.apache.hadoop.test.LambdaTestUtils;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mockito;
-
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 import static org.apache.hadoop.hdfs.protocol.SnapshotDiffReport.DiffType.MODIFY;
-import static org.apache.hadoop.hdfs.server.federation.FederationTestUtils.NAMENODES;
-import static org.apache.hadoop.hdfs.server.federation.MiniRouterDFSCluster.DEFAULT_HEARTBEAT_INTERVAL_MS;
-import static org.apache.hadoop.hdfs.server.federation.router.RBFConfigKeys.DFS_ROUTER_RPC_ASYNC_HANDLER_COUNT;
-import static org.apache.hadoop.hdfs.server.federation.router.RBFConfigKeys.DFS_ROUTER_RPC_ASYNC_RESPONDER_COUNT;
 import static org.apache.hadoop.hdfs.server.federation.router.async.utils.AsyncUtil.syncReturn;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-public class TestRouterAsyncSnapshot {
-  private static Configuration routerConf;
-  /** Federated HDFS cluster. */
-  private static MiniRouterDFSCluster cluster;
-  private static String ns0;
-
-  /** Random Router for this federated cluster. */
-  private MiniRouterDFSCluster.RouterContext router;
+/**
+ * Used to test the functionality of {@link RouterAsyncSnapshot}.
+ */
+public class TestRouterAsyncSnapshot extends RouterAsyncProtocolTestBase {
+  private final String testFile = "/testdir/testSnapshot.file";
   private FileSystem routerFs;
-  private RouterRpcServer routerRpcServer;
   private RouterAsyncSnapshot asyncSnapshot;
 
-  @BeforeClass
-  public static void setUpCluster() throws Exception {
-    cluster = new MiniRouterDFSCluster(true, 1, 2,
-        DEFAULT_HEARTBEAT_INTERVAL_MS, 1000);
-    cluster.setNumDatanodesPerNameservice(3);
-
-    cluster.startCluster();
-
-    // Making one Namenode active per nameservice
-    if (cluster.isHighAvailability()) {
-      for (String ns : cluster.getNameservices()) {
-        cluster.switchToActive(ns, NAMENODES[0]);
-        cluster.switchToStandby(ns, NAMENODES[1]);
-      }
-    }
-    // Start routers with only an RPC service
-    routerConf = new RouterConfigBuilder()
-        .rpc()
-        .build();
-
-    // Reduce the number of RPC clients threads to overload the Router easy
-    routerConf.setInt(RBFConfigKeys.DFS_ROUTER_CLIENT_THREADS_SIZE, 1);
-    routerConf.setInt(DFS_ROUTER_RPC_ASYNC_HANDLER_COUNT, 1);
-    routerConf.setInt(DFS_ROUTER_RPC_ASYNC_RESPONDER_COUNT, 1);
-    // We decrease the DN cache times to make the test faster
-    routerConf.setTimeDuration(
-        RBFConfigKeys.DN_REPORT_CACHE_EXPIRE, 1, TimeUnit.SECONDS);
-    cluster.addRouterOverrides(routerConf);
-    // Start routers with only an RPC service
-    cluster.startRouters();
-
-    // Register and verify all NNs with all routers
-    cluster.registerNamenodes();
-    cluster.waitNamenodeRegistration();
-    cluster.waitActiveNamespaces();
-    ns0 = cluster.getNameservices().get(0);
-  }
-
-  @AfterClass
-  public static void shutdownCluster() throws Exception {
-    if (cluster != null) {
-      cluster.shutdown();
-    }
-  }
-
   @Before
-  public void setUp() throws IOException {
-    router = cluster.getRandomRouter();
-    routerFs = router.getFileSystem();
-    routerRpcServer = router.getRouterRpcServer();
-    routerRpcServer.initAsyncThreadPool();
-    RouterAsyncRpcClient asyncRpcClient = new RouterAsyncRpcClient(
-        routerConf, router.getRouter(), routerRpcServer.getNamenodeResolver(),
-        routerRpcServer.getRPCMonitor(),
-        routerRpcServer.getRouterStateIdContext());
-    RouterRpcServer spy = Mockito.spy(routerRpcServer);
-    Mockito.when(spy.getRPCClient()).thenReturn(asyncRpcClient);
-    asyncSnapshot = new RouterAsyncSnapshot(spy);
-
-    // Create mock locations
-    MockResolver resolver = (MockResolver) router.getRouter().getSubclusterResolver();
-    resolver.addLocation("/", ns0, "/");
-    FsPermission permission = new FsPermission("705");
-    routerFs.mkdirs(new Path("/testdir"), permission);
+  public void setup() throws IOException {
+    routerFs = getRouterFs();
+    asyncSnapshot = new RouterAsyncSnapshot(getRouterAsyncRpcServer());
     FSDataOutputStream fsDataOutputStream = routerFs.create(
-        new Path("/testdir/testSnapshot.file"), true);
+        new Path(testFile), true);
     fsDataOutputStream.write(new byte[1024]);
     fsDataOutputStream.close();
-  }
-
-  @After
-  public void tearDown() throws IOException {
-    // clear client context
-    CallerContext.setCurrent(null);
-    boolean delete = routerFs.delete(new Path("/testdir"));
-    assertTrue(delete);
-    if (routerFs != null) {
-      routerFs.close();
-    }
   }
 
   @Test
